38ab92dd54166e3c0e7a0229e0c6776de6cfb279
aleks <aleks.mihaylov69@gmail.com>
2021-09-29 17:51:03 +0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(1);
+  const handleClick = () => {
+    setCount((count) => count + 1);
+  };
+
+  useEffect(() => {
+    const title = document.querySelector(".title");
+    title.textContent = `Count ${count}`;
+  }, [count]);
+
   return (
     <div className="App">
       <section class="hero">
         <div class="hero-body">
-          <p class="title">A React Task</p>
+          <p class="title">Count: ({count})</p>
           <p class="subtitle">by Boom.dev</p>
         </div>
       </section>
@@ -13,6 +24,7 @@ function App() {
         <div class="notification">
           Edit the <code>./src</code> folder to add components.
         </div>
+        <button onClick={handleClick}>Inctement count</button>
       </div>
     </div>
   );
